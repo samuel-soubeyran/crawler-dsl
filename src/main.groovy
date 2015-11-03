f52c08c81128b265473da52f9845800c84ef7b6b
Samuel Soubeyran <samuel.soubeyran@gmail.com>
2015-11-03 09:02:26 -0800

    improvement on the runner

--- src/main.groovy
+++ src/main.groovy
@@ -2,12 +2,42 @@
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 @Grapes( @Grab('org.jsoup:jsoup:1.6.1'))
+class Request {
+    String url;
+    public Request clone(){
+        def r = new Request()
+        r.url = this.url == null ? null : this.url.concat("")
+        return r
+    }
+}
+class Response {
+    String content;
+
+    Response clone(){
+        def r = new Response()
+        r.content = this.content == null ? null : this.content.concat("")
+        return r
+    }
+}
+class Message {
+    Request request;
+    Response response;
+    Object results;
+    public Message clone(){
+        def r = new Message()
+        r.response = this.response == null ? null : this.response.clone()
+        r.request = this.request == null ? null : this.request.clone()
+        r.results = this.results
+        return r
+    }
+}
+
+
 
 def cli = new CliBuilder()
 def options = cli.parse(args)
@@ -19,7 +49,10 @@ if(options.arguments().size()<2){
 }
 
 def crawlerfilename = options.arguments().first()
-def crawlerStartInput = options.arguments().get(1)
+def crawlerStartInputUrl = options.arguments().get(1)
+def crawlerStartInput = new Message()
+crawlerStartInput.request = new Request()
+crawlerStartInput.request.url = crawlerStartInputUrl
 
 
 class Emitted {
@@ -27,8 +60,25 @@ class Emitted {
     public void to(Closure sendTo){
         this.sendTo = sendTo
     }
-    public Object build(Object input){
-        return input
+    public List<Message> build(Message input){
+        def res = new ArrayList<>()
+        res.add(input.clone())
+        return res
+    }
+
+}
+
+class LinkEmitted extends  Emitted {
+
+    public List<Message> build(Message input){
+        def res = new ArrayList<>()
+        input.results.each { i ->
+            Message msg = new Message()
+            msg.request = new Request()
+            msg.request.url = i
+            res.add(msg)
+        }
+        return res
     }
 }
 
@@ -40,25 +90,30 @@ class ObjectEmitted extends Emitted {
         return this
     }
 
-    public Object build(Object input){
+    public List<Message> build(Message input){
         Map<Object, Object> res = new LinkedHashMap()
         this.fields.each { k, v ->
-            res.put(k, v.call(input))
+            res.put(k, v.call(input.results))
         }
-        return res
+        Message ret = input.clone()
+        ret.results = res
+        List<Message> rets = new ArrayList<>()
+        rets.add(ret)
+        return rets
     }
 }
 
 class Emitter {
     Emitted emitted
-    Closure action = { return it }
+    Closure action = {
+        println "Default emitter action closure call"
+        return it
+    }
     Emitter nextEmitter
+    Emitter previousEmitter
 
-    public Object getValue(Object input){
-        Object output = action(input)
-        if(emitted != null){
-            output = emitted.build(output)
-        }
+    public Message getValue(Message input){
+        Message output = action(input)
         if(nextEmitter != null){
             output = nextEmitter.getValue(output)
         }
@@ -70,6 +125,12 @@ class Emitter {
             return this
         }
         return this.nextEmitter.getLastEmitter()
+    }
+    public Emitter getFirstEmitter(){
+        if(this.previousEmitter == null){
+            return this;
+        }
+        return this.previousEmitter.getFirstEmitter();
     }
 }
 
@@ -84,7 +145,9 @@ abstract class Step {
         }
         currentEmitter.emitted = emittedFactory()
         outputs.add(currentEmitter)
-        return currentEmitter.emitted
+        def emitted = currentEmitter.emitted
+        currentEmitter = null
+        return emitted
     }
     public void recipe(Closure c){
         c.delegate = this
@@ -95,27 +158,157 @@ abstract class Step {
         Emitter e = new Emitter()
         e.action = closure
         if(currentEmitter != null){
+            e.previousEmitter = currentEmitter
             currentEmitter.nextEmitter = e
         }
         currentEmitter = e
     }
 
 
-    public abstract Object exec(Object s)
+    public void attributeSelector(String selector){
+        apply {
+            what ->
+                println "attributeSelector emitter action closure call with arg : " + selector
+                List<Object> obj = new ArrayList<>()
+                what.results.each { r ->
+                    if(r != null) {
+                        Document doc = Jsoup.parse(r)
+                        if (doc.body().children() != 0) {
+                            doc.body().children().each { e->
+                                if (e != null) {
+                                    obj.add(e.attr(selector))
+                                }
+                            }
+                        }
+                    }
+                }
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+    public void cssSelector(String selector){
+        apply {
+            what ->
+                println "cssSelector emitter action closure call with arg : " + selector
+                List<Object> obj = new ArrayList<>()
+                what.results.each { r ->
+                    if(r != null) {
+                        Document doc = Jsoup.parse(r)
+                        doc.select(selector).each { e ->
+                            obj.add(e.outerHtml())
+                        };
+                    }
+                }
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+    public void regexp(String regexp){
+        apply {
+            what ->
+                println "regexp emitter action closure call with arg : " + regexp
+                List<Object> obj = new ArrayList<>()
+                what.results.each { r ->
+                    Pattern p = Pattern.compile(regexp);
+                    Matcher m = p.matcher(r);
+                    while(m.find()){
+                        obj.add(m.group())
+                    }
+                }
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+    public void extractCrawledUrl(){
+        apply {
+            what ->
+                println "extractCrawledUrl emitter action closure call "
+                List<Object> obj = new ArrayList<>()
+                what.results.each {
+                    obj.add(what.request.url)
+                }
+
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+    public void toFirstElement(){
+        apply {
+            what ->
+                println "toFirstElement emitter action closure call"
+                def ret = ((Message)what).clone()
+                ret.results = what.results.get(0)
+                return ret
+        }
+    }
+
+    public void textCssSelector(String selector){
+        apply {
+            what ->
+                println "textCssSelector emitter action closure call with arg : " + selector
+                List<Object> obj = new ArrayList<>()
+                what.results.each { r ->
+                    if(r != null) {
+                        Document doc = Jsoup.parse(r)
+                        doc.select(selector).each { e ->
+                            obj.add(e.text())
+                        };
+                    }
+                }
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+    public void prepend(String pre){
+        apply {
+            what ->
+                println "prepend emitter action closure call with arg : " + pre
+                List<Object> obj = new ArrayList<>()
+                what.results.each { r ->
+                    if(r != null){
+                        obj.add(pre.concat(r.toString()))
+                    }
+                }
+                def ret = ((Message)what).clone()
+                ret.results = obj
+                return ret
+        }
+    }
+
+
+    public abstract Object exec(Message s)
 }
 
 class Download extends Step {
 
-    public Object exec(Object input){
-        println "downloading url " + input
-        "aaaaaaabbbbbbbbccccccccdddddddddeeeeeeefffffffffff"
+    public Object exec(Message input){
+        println "downloading url " + input.request.url
+        Message m = (Message) input;
+        Message result = input.clone();
+        result.response = new Response()
+        result.response.content = new URL(m.request.url).getText()
+        return result
     }
 }
 
 class Parse extends Step {
-    public Object exec(Object input){
-        println "parsing content " + input
-        input
+    public Object exec(Message input){
+        println "parsing content " + input.response.content
+        List<Object> array = new ArrayList()
+        array.add(input == null || input.response == null || input.response.content == null ? "" : input.response.content.concat(""))
+        Message r = input.clone()
+        r.results = array
+        return r;
     }
 }
 
@@ -124,7 +317,7 @@ abstract class Crawlerfile extends Script {
     LinkedHashMap<String, Object> parse = new LinkedHashMap<>()
 
     def getStartStepClosure = {}
-    def link = { return new Emitted() }
+    def link = { return new LinkEmitted() }
     def page = { return new Emitted() }
     def object = { return new ObjectEmitted() }
 
@@ -156,63 +349,6 @@ abstract class Crawlerfile extends Script {
         getStartStepClosure = getStartStep
     }
 
-    public Closure attributeSelector(String selector){
-        return {
-            what ->
-                List<Object> obj = new ArrayList<>()
-                what.results.each { r ->
-                    if(r != null) {
-                        Document doc = Jsoup.parse(r)
-                        if (doc.body().children() != 0) {
-                            Element e = doc.body().child(0)
-                            if (e != null) {
-                                obj.add(e.attr(selector))
-                            }
-                        }
-                    }
-                }
-                what.results = obj;
-        }
-    }
-
-    public Closure regexp(String regexp){
-        return {
-            what ->
-                List<Object> obj = new ArrayList<>()
-                what.results.each { r ->
-                    Pattern p = Pattern.compile(regexp);
-                    Matcher m = p.matcher(r);
-                    while(m.find()){
-                        obj.add(m.group())
-                    }
-                }
-                what.results = obj;
-        }
-    }
-
-    public Closure extractCrawledUrl(){
-        return {
-            what ->
-                List<Object> obj = new ArrayList<>()
-                what.results.each {
-                    obj.add(what.request.url)
-                }
-                what.results = obj
-        }
-    }
-
-    public Closure toFirstElement(){
-        return {
-            what ->
-                what.results = what.results.get(0)
-        }
-    }
-
-    public Closure textCssSelector(String selector){
-        
-    }
-
-
 }
 
 def crawlerConfig = new CompilerConfiguration();
@@ -226,7 +362,7 @@ def crawlerfile = shell.evaluate(crawlerfileContents + "\nreturn this\n")
 
 public class StepExecution{
     Step step
-    Object input
+    Message input
 }
 
 public class CrawlerfileExecutor {
@@ -235,24 +371,30 @@ public class CrawlerfileExecutor {
         StepExecution exec = new StepExecution()
         exec.input = crawlerStartInput
         exec.step = crawlerfile.getStartStep()
-        List<StepExecution> steps = new ArrayList<>()
-        steps.add(exec)
+        LinkedList<StepExecution> steps = new LinkedList<>()
+        steps.push(exec)
         while(steps.size() > 0){
-            for(StepExecution stepExecution : steps){
-                steps.remove(stepExecution);
-                steps.addAll(runStep(stepExecution))
+            runStep(steps.pop()).each {
+                s -> steps.push(s)
             }
         }
     }
     public static List<StepExecution> runStep(StepExecution current){
-        String output = current.step.exec(current.input)
+        Message output = current.step.exec(current.input)
         def res = new ArrayList()
         List<Emitter> emitters = current.step.getOutputs()
         for(Emitter e : emitters){
-            StepExecution stepExecution = new StepExecution()
-            stepExecution.step = e.getLastEmitter().getEmitted().sendTo();
-            stepExecution.input = e.getValue(output)
-            res.add(stepExecution)
+            List<StepExecution> stepExecutions = new ArrayList<>()
+            def msg = e.getFirstEmitter().getValue(output)
+            def step = e.getLastEmitter().getEmitted().sendTo();
+            List<Message> lmsg = e.getLastEmitter().emitted.build(msg)
+            lmsg.each { msgitem ->
+                StepExecution exec = new StepExecution()
+                exec.input = msgitem
+                exec.step = step
+                stepExecutions.add(exec)
+            }
+            res.addAll(stepExecutions)
         }
         return res
     }
